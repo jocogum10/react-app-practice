c21bb94123b223016bbec544de8cec9f55611d1e
Joco Gum <jocogum10@gmail.com>
2022-06-20 20:43:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+import logo from './avion-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Avion School React Class</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+          Let's start changing the <code>src/App.js</code>!.
+        </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
